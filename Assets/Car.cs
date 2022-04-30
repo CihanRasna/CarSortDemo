c14ca4a285c96706156a781cd0546feae567510c
Cihan Rasna <cihan.rasna@vanta.games>
2022-04-30 12:26:26 +0300

    Test assets imported

---
+++ Assets/Car.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    public Transform point;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
